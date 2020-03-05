589169ead6a18a84f2c5670bd35414728bce67b8
yang <623522656@qq.com>
2020-03-05 11:34:00 +0800

    初始构建

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <s-head></s-head>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>
+        <s-foot></s-foot>
+      </el-footer>
+    </el-container>
+  </div>
+</template> 
+
+<script>
+
+import SHead from './components/SHead.vue'
+import SFoot from './components/SFoot.vue'
+
+export default {
+  name: 'App',
+  components: {
+    's-head': SHead,
+    's-foot': SFoot
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SHead.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <el-row>
+            <el-menu
+              :default-active="activeIndex"
+              class="el-menu-demo"
+              mode="horizontal"
+              @select="handleSelect"
+              background-color="#545c64"
+              text-color="#fff"
+              active-text-color="#ffd04b">
+              <el-menu-item index="1">11111111</el-menu-item>
+              <el-menu-item index="2">22222222</el-menu-item>
+              <el-menu-item index="3">33333333</el-menu-item>
+              <el-menu-item index="4">44444444</el-menu-item>
+            </el-menu>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SHead',
+    data: function() {
+        return {
+            activeIndex: '1',
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    },
+}
+</script>
